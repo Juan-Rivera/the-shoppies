6196d3adb0804805d864dbaa2a78830c004d4ba2
Juan Rivera <juanrivera.primary@gmail.com>
2021-05-06 15:20:47 -0400

    hero section built out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Hero from './components/Hero.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import shopifyLogo from '../images/shopify-logo.png'
+import heroDown from '../images/chevron-down.png';
+import navButton1 from '../images/nav-button-1.png';
+
+const Hero = () => {
+
+    return (
+        <div>
+            <header>
+                <img src={shopifyLogo} alt='shopify-logo' width="299" height="188" />
+                <h1>The Shoppies</h1>
+            </header>
+            <div className="hero-footer">
+                <div className="cta">
+                    <p>Select Your Top 5 Films</p>
+                    <img src={heroDown} alt="navigation down" />
+                </div>
+                <div className="right-nav nav-buttons">
+                    <img src={navButton1} alt="navigation down" />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Hero;
